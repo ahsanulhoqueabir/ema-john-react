de46f186a1b9a05fd38fa3d224e27066bca1cd73
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2023-11-20 02:51:06 +0600

    db updated

--- src/components/shops/Shops.jsx
+++ src/components/shops/Shops.jsx
@@ -1,3 +1,4 @@
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../cart/Cart';
 import Product from '../product/Product';
 import './Shops.css'
@@ -10,14 +11,22 @@ const Shops = () => {
     const [cart,setCart] = useState([]);
     useEffect(()=>
     {
-        fetch('https://raw.githubusercontent.com/ahsanulhoqueabir/ema-john-react/main/src/fakeData/products.json').then(response => response.json()).then(data => setProduct(data))
+        // fetch('https://raw.githubusercontent.com/ahsanulhoqueabir/ema-john-react/main/src/fakeData/products.json').then(response => response.json()).then(data => setProduct(data))
+        fetch('products.json').then(response => response.json()).then(data => setProduct(data))
 
+    },[]);
+
+    useEffect(()=>
+    {
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
     },[]);
 
     const ProductAdd = (product)=> {
         
         let newCart= [...cart,product]
         setCart(newCart)
+        addToDb(product.id)
     }
 
 
