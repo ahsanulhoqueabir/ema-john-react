b772d04586c018be91164bf11f01608bc3c8aa24
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2023-11-22 20:51:37 +0600

    retrive data from local storage

--- src/components/shops/Shops.jsx
+++ src/components/shops/Shops.jsx
@@ -11,16 +11,43 @@ const Shops = () => {
     const [cart,setCart] = useState([]);
     useEffect(()=>
     {
-        // fetch('https://raw.githubusercontent.com/ahsanulhoqueabir/ema-john-react/main/src/fakeData/products.json').then(response => response.json()).then(data => setProduct(data))
-        fetch('products.json').then(response => response.json()).then(data => setProduct(data))
+        fetch('https://raw.githubusercontent.com/ahsanulhoqueabir/ema-john-react/main/public/products.json').then(response => response.json()).then(data => setProduct(data))
+        // fetch('products.json').then(response => response.json()).then(data => setProduct(data))
 
     },[]);
 
-    useEffect(()=>
-    {
+    // useEffect(()=>
+    // {
+    //     const storedCart = getShoppingCart();
+    //     // step-1: get id 
+    //     for(const id in storedCart)
+    //     {
+    //         // step-2: get the product by using id 
+    //         const setPro = product.find(p => p.id  === id)
+    //         // step 3: quantity ofproducts 
+    //         const x = storedCart[id]
+    //         // setPro.quantity = x;
+    //         // console.log(x);
+    //         console.log(setPro);
+    //         console.log(setPro.quantity)
+    //     }
+    // },[product]);
+    useEffect(()=>{
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    },[]);
+        let savedcart = [];
+        for(let id in storedCart)
+        {
+            const addedProduct = product.find(p => p.id === id)
+            // console.log(addedProduct);
+            if(addedProduct)
+            {
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity;
+                savedcart.push(addedProduct)
+            }
+        }
+        setCart(savedcart)
+    },[product])
 
     const ProductAdd = (product)=> {
         
